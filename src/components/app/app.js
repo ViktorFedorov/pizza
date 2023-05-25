93cca8f1298fc02939b1ad3d1f6e8f85de282a70
ViktorF <logforpractik@yandex.ru>
2023-05-25 12:46:30 +0300

    cards layout done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,7 +11,6 @@ function App() {
       <Header />
       <FilterPanel />
       <Cards />
-      <CardItem />
     </div>
   )
 }
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from './cards.module.css'
+import CardItem from '../card-item/card-item'
+
+const fakeData = [1, 1, 1, 1, 1, 1, 1, 1]
 
 const Cards = () => {
   return (
     <div>
       <h1 className={styles.heading}>Все пиццы</h1>
-      <ul></ul>
+      <ul className={styles.list}>
+        {fakeData.map(() => (
+          <CardItem />
+        ))}
+      </ul>
     </div>
   )
 }
