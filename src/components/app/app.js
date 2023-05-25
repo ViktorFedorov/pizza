6c28889b849effa797b5b698485920119c71850c
ViktorF <logforpractik@yandex.ru>
2023-05-25 08:37:57 +0300

    cards component proto

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Header from '../header/header'
-import styles from './app.module.css'
 import FilterPanel from '../filter-panel/filter-panel'
 import CardItem from '../card-item/card-item'
+import Cards from '../cards/cards'
+import styles from './app.module.css'
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
       <FilterPanel />
+      <Cards />
       <CardItem />
     </div>
   )
---
+++ src/components/cards/cards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './cards.module.css'
+
+const Cards = () => {
+  return (
+    <div>
+      <h1 className={styles.heading}>Все пиццы</h1>
+      <ul></ul>
+    </div>
+  )
+}
+
+export default Cards
