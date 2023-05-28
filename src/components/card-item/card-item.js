f90ad697c808c07d25eab78531e029e2d43ec105
ViktorF <logforpractik@yandex.ru>
2023-05-28 09:10:17 +0300

    pizza-counter done

--- src/components/card-item/card-item.js
+++ src/components/card-item/card-item.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import pizzaImage from '../../images/item1.jpg'
 import styles from './card-item.module.css'
 
 const CardItem = () => {
+  const [quantity, setQuantity] = useState(0)
+
   return (
     <li className={styles.card}>
       <img className={styles.img} src={pizzaImage} alt='' />
       <h3 className={styles.heading}>Чизбургер-пицца</h3>
-
       <div className={styles.settings}>
         <ul className={styles.base}>
           <li className={styles.active}>тонкое</li>
           <li>традиционное</li>
         </ul>
-
         <ul className={styles.size}>
           <li>26 см.</li>
           <li>30 см.</li>
@@ -22,9 +22,12 @@ const CardItem = () => {
       </div>
       <div className={styles.cardFooter}>
         <div className={styles.price}>от 395 ₽</div>
-        <button className={styles.add}>
+        <button
+          onClick={() => setQuantity(quantity + 1)}
+          className={styles.add}
+        >
           Добавить
-          <div className={styles.counter}>3</div>
+          {!!quantity && <div className={styles.counter}>{quantity}</div>}
         </button>
       </div>
     </li>
