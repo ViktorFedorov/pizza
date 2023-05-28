794ec1d533b807088e01e7fdb851e939603241ab
ViktorF <logforpractik@yandex.ru>
2023-05-28 11:38:29 +0300

    loader

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import CardItem from '../card-item/card-item'
 import { getPizzaList } from '../../utils/api'
 import styles from './cards.module.css'
+import SpinnerLoader from '../spinner-loader/spinner-loader'
 
 const Cards = () => {
   const [list, setList] = useState([])
@@ -17,11 +18,15 @@ const Cards = () => {
   return (
     <div>
       <h1 className={styles.heading}>Все пиццы</h1>
-      <ul className={styles.list}>
-        {list.length
-          ? list.map((item) => <CardItem key={item.title} {...item} />)
-          : null}
-      </ul>
+      {!loading ? (
+        <ul className={styles.list}>
+          {list.map((item) => (
+            <CardItem key={item.title} {...item} />
+          ))}
+        </ul>
+      ) : (
+        <SpinnerLoader />
+      )}
     </div>
   )
 }
